871d2421ba11f04bc64fd608dafab93b3ab381f2
GrigstonJC <jgrigston@gmail.com>
2024-12-10 01:06:12 -0500

    use gnu versions

--- nix/flake.nix
+++ nix/flake.nix
@@ -192,12 +192,34 @@
 			};
 			zsh = {
 				enable = true;
-				initExtra = "source ~/.p10k.zsh";
-				plugins = [   
-					{                                                                                   
-						name = "powerlevel10k";                                                           
-						src = pkgs.zsh-powerlevel10k;                                                     
-						file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";                         
+				initExtra = ''
+					BREW_BIN="/usr/local/bin/brew"
+					if [ -f "/opt/homebrew/bin/brew" ]; then
+						BREW_BIN="/opt/homebrew/bin/brew"
+					fi
+
+					if type "''${BREW_BIN}" &> /dev/null; then
+						export BREW_PREFIX="$("''${BREW_BIN}" --prefix)"
+						for bindir in "''${BREW_PREFIX}/opt/"*"/libexec/gnubin"; do
+							export PATH=$bindir:$PATH
+						done
+						for bindir in "''${BREW_PREFIX}/opt/"*"/bin"; do
+							export PATH=$bindir:$PATH
+						done
+						for mandir in "''${BREW_PREFIX}/opt/"*"/libexec/gnuman"; do
+							export MANPATH=$mandir:$MANPATH
+						done
+						for mandir in "''${BREW_PREFIX}/opt/"*"/share/man/man1"; do
+							export MANPATH=$mandir:$MANPATH
+						done
+					fi
+					source ~/.p10k.zsh
+				'';
+				plugins = [
+					{
+						name = "powerlevel10k";
+						src = pkgs.zsh-powerlevel10k;
+						file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
 					}
 				];
 				oh-my-zsh = {
