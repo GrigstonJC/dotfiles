bde1b95782bcd997b30c5991106fee3b37528ec7
GrigstonJC <jgrigston@gmail.com>
2026-02-23 22:06:03 -0500

    remap flash jump

--- nix/lazyvim/lua/config/keymaps.lua
+++ nix/lazyvim/lua/config/keymaps.lua
@@ -4,3 +4,8 @@ vim.g.mapleader = " "
 
 -- Restore default vim 's' behavior
 vim.keymap.set("n", "s", "s")
+
+-- Flash jump on <Space>j
+vim.keymap.set("n", "<leader>j", function()
+	require("flash").jump()
+end, { desc = "Flash Jump" })
